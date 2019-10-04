21ba4d96ebf8506280fa6ca69e9d2ea0497646d0
UBC Student <knox777@students.cs.ubc.ca>
2019-10-04 13:03:44 -0700

    project_path.hpp should not be commited

--- src/project_path.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Please don't change the content of this header
-
-#define PROJECT_SOURCE_DIR "C:/Users/Jonathan/Documents/John/UBC/2019W1/CPSC 427/project/team repo/tileMap2/team2/"
