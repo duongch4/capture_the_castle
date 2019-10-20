d818b812e5479495edbcb8f388f4d92bff30f2d1
Bang Chi Duong <chibangduong1993@gmail.com>
2019-10-20 00:36:04 -0700

    Handled idle-time-out and chase-time-out; Refactor with pass-by-reference to speedup a bit

--- src/systems/bandit_ai_system.cpp
+++ src/systems/bandit_ai_system.cpp
@@ -5,67 +5,119 @@
 #include "bandit_ai_system.hpp"
 
 
-bool BanditAISystem::init(Entity player_1, Entity player_2) {
-	m_currentState = State::IDLE;
+bool BanditAISystem::init(Entity& player_1, Entity& player_2)
+{
 	m_targets.emplace_back(player_1);
 	m_targets.emplace_back(player_2);
+	for (size_t i = 0; i < MAX_BANDITS; ++i)
+	{
+		m_idleTimes.emplace_back(0);
+		m_chaseTimes.emplace_back(0);
+		m_states.emplace_back(State::IDLE);
+	}
 	return true;
 }
 
-void BanditAISystem::update(float elapsed_ms) {
+void BanditAISystem::update(float& elapsed_ms)
+{
+	for (auto it = entities.begin(); it != entities.end(); ++it)
+	{
+		auto idx = std::distance(entities.begin(), it);
+		Entity bandit = *it;
+		State& state = m_states[idx];
+		size_t& idleTime = m_idleTimes[idx];
+		size_t& chaseTime = m_chaseTimes[idx];
+		float speed = BASE_SPEED * (1.f + dist(rng));
+		//std::cout << speed << std::endl;
 
-	for (auto i = entities.begin(); i != entities.end(); i++) {
-		speed = 70.f * (1.f + dist(rng));
-		m_bandit = *i;
+		float distance_1 = getDistance(m_targets[0], bandit);
+		float distance_2 = getDistance(m_targets[1], bandit);
 
-		float distance_1 = getDistance(m_targets[0], m_bandit);
-		float distance_2 = getDistance(m_targets[1], m_bandit);
-
-		switch (m_currentState) {
+		switch (state)
+		{
 		case State::IDLE:
-			//if (next_bandit_spawn < 2.0f) {
-			//	m_currentState = CHASE;
-			//}
-			ecsManager.getComponent<Motion>(m_bandit).direction = { 0,0 };
+			handleIdle(state, idleTime, chaseTime, distance_1, distance_2, bandit, speed, elapsed_ms);
 			break;
 		case State::CHASE:
-			//std::cout << (getDistance(m_target, m_bandit) > CHASE_THRESHOLD) << std::endl;
-			if (
-				(distance_1 > CHASE_THRESHOLD) && (distance_2 > CHASE_THRESHOLD)
-				) {
-				m_currentState = State::IDLE;
-				return;
-			}
-			if (distance_1 < distance_2) {
-				followDirection(m_targets[0], m_bandit, elapsed_ms);
-			}
-			else {
-				followDirection(m_targets[1], m_bandit, elapsed_ms);
-			}
+			handleChase(state, idleTime, chaseTime, distance_1, distance_2, bandit, speed, elapsed_ms);
 			break;
 		}
-		checkTarget(distance_1, distance_2);
 	}
 }
 
-void BanditAISystem::checkTarget(float distance_1, float distance_2) {
-	if (m_currentState == State::CHASE) {
+void BanditAISystem::handleIdle(
+	State& state, size_t& idle_time, size_t& chase_time,
+	float& distance_1, float& distance_2,
+	Entity& bandit, float& speed, float& elapsed_ms
+)
+{
+	if (canChase(distance_1, distance_2, idle_time))
+	{
+		state = State::CHASE;
+		chase_time = 0;
 		return;
 	}
 
-	if ((distance_1 < CHASE_THRESHOLD) || (distance_2 < CHASE_THRESHOLD)) {
-		m_currentState = State::CHASE;
+	ecsManager.getComponent<Motion>(bandit).direction = { 0,0 };
+	idle_time++;
+}
+
+bool BanditAISystem::canChase(float& distance_1, float& distance_2, size_t& idle_time)
+{
+	return (
+		((distance_1 < CHASE_THRESHOLD) || (distance_2 < CHASE_THRESHOLD)) &&
+		(idle_time > IDLE_LIMIT)
+		);
+}
+
+void BanditAISystem::handleChase(
+	State& state, size_t& idle_time, size_t& chase_time,
+	float& distance_1, float& distance_2,
+	Entity& bandit, float& speed, float& elapsed_ms
+)
+{
+	std::cout << chase_time << std::endl;
+	if (cannotChase(distance_1, distance_2, chase_time))
+	{
+		state = State::IDLE;
+		idle_time = 0;
+		return;
+	}
+
+	chase(distance_1, distance_2, bandit, speed, elapsed_ms);
+	chase_time++;
+}
+
+bool BanditAISystem::cannotChase(float& distance_1, float& distance_2, size_t& chase_time)
+{
+	return (
+		((distance_1 > CHASE_THRESHOLD) && (distance_2 > CHASE_THRESHOLD)) ||
+		(chase_time > CHASE_LIMIT)
+		);
+}
+
+void BanditAISystem::chase(float& distance_1, float& distance_2, Entity& bandit, float& speed, float& elapsed_ms)
+{
+	if (distance_1 < distance_2)
+	{
+		followDirection(m_targets[0], bandit, speed, elapsed_ms);
+	}
+	else
+	{
+		followDirection(m_targets[1], bandit, speed, elapsed_ms);
 	}
 }
 
-float BanditAISystem::getDistance(Entity target, Entity bandit) {
+float BanditAISystem::getDistance(Entity& target, Entity& bandit)
+{
 	vec2 target_transform_pos = ecsManager.getComponent<Transform>(target).position;
 	vec2 bandit_transform_pos = ecsManager.getComponent<Transform>(bandit).position;
 	vec2 difference = { target_transform_pos.x - bandit_transform_pos.x, target_transform_pos.y - bandit_transform_pos.y };
 	return std::sqrtf((difference.x * difference.x) + (difference.y * difference.y));
 }
 
-void BanditAISystem::followDirection(Entity target, Entity bandit, float elapsed_ms) {
+void BanditAISystem::followDirection(Entity& target, Entity& bandit, float& speed, float& elapsed_ms)
+{
 	vec2& target_transform_pos = ecsManager.getComponent<Transform>(target).position;
 	vec2& bandit_transform_pos = ecsManager.getComponent<Transform>(bandit).position;
 	vec2& target_motion_dir = ecsManager.getComponent<Motion>(target).direction;
@@ -75,13 +127,15 @@ void BanditAISystem::followDirection(Entity target, Entity bandit, float elapsed
 
 	if (
 		isTargetMoveTowardBandit(bandit_transform_pos, target_transform_pos, target_motion_dir)
-	) {
+		)
+	{
 		bandit_transform_pos.x -= target_motion_dir.x * step;
 		bandit_transform_pos.y -= target_motion_dir.y * step;
 	}
 	if (
 		isTargetMoveAwayBandit(bandit_transform_pos, target_transform_pos, target_motion_dir)
-	) {
+		)
+	{
 		bandit_transform_pos.x += target_motion_dir.x * step;
 		bandit_transform_pos.y += target_motion_dir.y * step;
 	}
@@ -90,30 +144,32 @@ void BanditAISystem::followDirection(Entity target, Entity bandit, float elapsed
 	float dir_x = target_transform_pos.x - bandit_transform_pos.x;
 	float dir_y = target_transform_pos.y - bandit_transform_pos.y;
 	bandit_motion_dir = { dir_x / distance, dir_y / distance };
-	std::cout << "x" << std::endl;
-	std::cout << bandit_motion_dir.x << std::endl;
-	std::cout << "y" << std::endl;
-	std::cout << bandit_motion_dir.y << std::endl;
+	//std::cout << "x" << std::endl;
+	//std::cout << bandit_motion_dir.x << std::endl;
+	//std::cout << "y" << std::endl;
+	//std::cout << bandit_motion_dir.y << std::endl;
 }
 
 bool BanditAISystem::isTargetMoveTowardBandit(
-	vec2 bandit_transform_pos, vec2 target_transform_pos, vec2 target_motion_dir
-) {
+	vec2& bandit_transform_pos, vec2& target_transform_pos, vec2& target_motion_dir
+)
+{
 	return (
 		((bandit_transform_pos.x < target_transform_pos.x) && (target_motion_dir.x < 0.f)) ||
 		((bandit_transform_pos.x > target_transform_pos.x) && (target_motion_dir.x > 0.f)) ||
 		((bandit_transform_pos.y < target_transform_pos.y) && (target_motion_dir.y < 0.f)) ||
 		((bandit_transform_pos.y > target_transform_pos.y) && (target_motion_dir.y > 0.f))
-	);
+		);
 }
 
 bool BanditAISystem::isTargetMoveAwayBandit(
-	vec2 bandit_transform_pos, vec2 target_transform_pos, vec2 target_motion_dir
-) {
+	vec2& bandit_transform_pos, vec2& target_transform_pos, vec2& target_motion_dir
+)
+{
 	return (
 		((bandit_transform_pos.x < target_transform_pos.x) && (target_motion_dir.x > 0.f)) ||
 		((bandit_transform_pos.x > target_transform_pos.x) && (target_motion_dir.x < 0.f)) ||
 		((bandit_transform_pos.y < target_transform_pos.y) && (target_motion_dir.y > 0.f)) ||
 		((bandit_transform_pos.y > target_transform_pos.y) && (target_motion_dir.y < 0.f))
-	);
+		);
 }
--- src/systems/bandit_ai_system.hpp
+++ src/systems/bandit_ai_system.hpp
@@ -16,28 +16,50 @@
 
 extern ECSManager ecsManager;
 
-class BanditAISystem: public System {
+class BanditAISystem : public System
+{
 public:
-	bool init(Entity player_1, Entity player_2);
-    void update(float ms);
+	bool init(Entity& player_1, Entity& player_2);
+	void update(float& ms);
 
 private:
-	enum struct State {
+	enum struct State
+	{
 		IDLE,
 		CHASE,
 	};
 
+	const size_t MAX_BANDITS = 5;
+	const size_t IDLE_LIMIT = 30; // beats
 	const size_t CHASE_THRESHOLD = 200;
-	float speed = 70.f;
+	const size_t CHASE_LIMIT = 100; // beats
+	const float BASE_SPEED = 70.f;
 
-	State m_currentState;
-	Entity m_bandit;
+	std::vector<size_t> m_idleTimes;
+	std::vector<size_t> m_chaseTimes;
+	std::vector<State> m_states;
+	std::vector<Entity> m_bandits;
 	std::vector<Entity> m_targets;
-	void checkTarget(float distance_1, float distance_2);
-	float getDistance(Entity target, Entity bandit);
-	void followDirection(Entity target, Entity bandit, float elapsed_ms);
-	bool isTargetMoveTowardBandit(vec2 bandit_transform_pos, vec2 target_transform_pos, vec2 target_motion_dir);
-	bool isTargetMoveAwayBandit(vec2 bandit_transform_pos, vec2 target_transform_pos, vec2 target_motion_dir);
+
+	float getDistance(Entity& target, Entity& bandit);
+
+	void handleIdle(
+		State& state, size_t& idle_time, size_t& chase_time,
+		float& distance_1, float& distance_2,
+		Entity& bandit, float& speed, float& elapsed_ms
+	);
+	bool canChase(float& distance_1, float& distance_2, size_t& idle_time);
+
+	void handleChase(
+		State& state, size_t& idle_time, size_t& chase_time,
+		float& distance_1, float& distance_2,
+		Entity& bandit, float& speed, float& elapsed_ms
+	);
+	bool cannotChase(float& distance_1, float& distance_2, size_t& chase_time);
+	void chase(float& distance_1, float& distance_2, Entity& bandit, float& speed, float& elapsed_ms);
+	void followDirection(Entity& target, Entity& bandit, float& speed, float& elapsed_ms);
+	bool isTargetMoveTowardBandit(vec2& bandit_transform_pos, vec2& target_transform_pos, vec2& target_motion_dir);
+	bool isTargetMoveAwayBandit(vec2& bandit_transform_pos, vec2& target_transform_pos, vec2& target_motion_dir);
 
 	// C++ rng
 	std::default_random_engine rng;
